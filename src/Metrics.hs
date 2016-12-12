3c643b8e826ad8edc6fd36ded08bf815daa64aa5
Dan Piponi <dpiponi@gmail.com>
2016-12-12 09:25:19 -0800

    Added Metrics

---
+++ src/Metrics.hs
@@ -0,0 +1,19 @@
+module Metrics where
+
+import Data.Word
+import Foreign.C.Types
+
+-- Some screen-related metrics
+screenScanLines :: CInt
+screenScanLines = 192+40+20
+picy :: CInt
+picy = 40-40
+picx :: CInt
+picx = 68
+
+screenWidth, screenHeight :: CInt
+(screenWidth, screenHeight) = (160, screenScanLines)
+
+xscale, yscale :: CInt
+xscale = 5
+yscale = 3
