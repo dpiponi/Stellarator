5f2e4c8b0d9b8f39f2c36ee01050eb0bcc1e338b
Dan Piponi <dpiponi@gmail.com>
2017-01-22 10:38:50 -0800

    More case expansion 20

--- src/Core.hs
+++ src/Core.hs
@@ -1121,7 +1121,7 @@ step = do
         0x18 -> ins_set putC False
         0x19 -> op_ora 0b110
         0x1d -> op_ora 0b111
-        0x1e -> op_asl 0b111 --
+        0x1e -> op_asl 0b111
         0x20 -> ins_jsr
         0x21 -> op_and 0b000
         0x25 -> op_and 0b001
@@ -1136,6 +1136,7 @@ step = do
         0x38 -> ins_set putC True
         0x39 -> op_and 0b110
         0x3d -> op_and 0b111
+        0x3e -> op_rol 0b111
         0x40 -> ins_rti
         0x41 -> op_xor 0b000
         0x45 -> op_xor 0b001
@@ -1150,6 +1151,7 @@ step = do
         0x58 -> ins_set putI False
         0x59 -> op_xor 0b110
         0x5d -> op_xor 0b111
+        0x5e -> op_lsr 0b111
         0x60 -> ins_rts
         0x61 -> op_adc 0b000
         0x65 -> op_adc 0b001
@@ -1164,6 +1166,7 @@ step = do
         0x78 -> ins_set putI True
         0x79 -> op_adc 0b110
         0x7d -> op_adc 0b111
+        0x7e -> op_ror 0b111
         0x81 -> op_sta 0b000
         0x85 -> op_sta 0b001
         0x86 -> op_stx 0b001
@@ -1190,6 +1193,7 @@ step = do
         0xb9 -> op_lda 0b110
         0xba -> ins_transfer getS putX
         0xbd -> op_lda 0b111
+        0xbe -> op_ldx 0b111
         0xc1 -> op_cmp 0b000
         0xc5 -> op_cmp 0b001
         0xc6 -> op_dec 0b001
@@ -1202,6 +1206,7 @@ step = do
         0xd8 -> ins_set putD False
         0xd9 -> op_cmp 0b110
         0xdd -> op_cmp 0b111
+        0xde -> op_dec 0b111
         0xe1 -> op_sbc 0b000
         0xe5 -> op_sbc 0b001
         0xe6 -> op_inc 0b001
@@ -1214,6 +1219,7 @@ step = do
         0xf8 -> ins_set putD True
         0xf9 -> op_sbc 0b110
         0xfd -> op_sbc 0b111
+        0xfe -> op_inc 0b111
 
         _ -> do
             let cc = i .&. 0b11
