c17805be10dce8b811688460dad885f44f55316e
Dan Piponi <dpiponi@gmail.com>
2017-01-21 22:18:40 -0800

    More case expansion 9

--- src/Core.hs
+++ src/Core.hs
@@ -1116,6 +1116,7 @@ step = do
         0x0d -> op_ora 0b011
         0x0e -> op_asl 0b011
         0x10 -> ins_bra getN False
+        0x15 -> op_ora 0b101 -- ??
         0x18 -> ins_set putC False
         0x20 -> ins_jsr
         0x21 -> op_and 0b000
