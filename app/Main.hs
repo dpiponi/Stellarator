6d9a5d813defb98088309eeb02f280e06dfc1e57
Dan Piponi <dpiponi@gmail.com>
2018-01-27 16:56:32 -0800

    Pulled delay list out to top. Must consider making adustment of delays interactive.

--- app/Main.hs
+++ app/Main.hs
@@ -40,6 +40,48 @@ clargs = Args { file = "adventure.bin",
                 options = ".stellarator-options",
                 debugStart = False }
 
+delayList :: [(Word16, Int)]
+delayList =  [
+                (0x00, 0), -- VSYNC
+                (0x01, 0), -- VBLANK
+                (0x02, 0), -- WSYNC
+                (0x03, 0), -- NUSIZ0
+                (0x05, 4), -- NUSIZ1
+                (0x06, 0), -- COLUP0
+                (0x07, 0), -- COLUP0
+                (0x08, 0), -- COLUPF
+                (0x09, 0), -- COLUBK
+                (0x0a, 0), -- CTRLPF
+                (0x0b, 0), -- REFP0
+                (0x0c, 0), -- REFP1
+                (0x0d, 3), -- PF0
+                (0x0e, 3), -- PF1
+                (0x0f, 3), -- PF2
+                (0x10, 5), -- RESP0
+                (0x11, 5), -- RESP1
+                (0x12, 4), -- RESM0
+                (0x13, 4), -- RESM1
+                (0x14, 4), -- RESBL
+                (0x1b, 1), -- GRP0
+                (0x1c, 1), -- GRP1
+                (0x1d, 0), -- ENAM0
+                (0x1e, 0), -- ENAM1
+                (0x1f, 0), -- ENABL
+                (0x20, 0), -- HMP0
+                (0x21, 0), -- HMP1
+                (0x22, 0), -- HMM0
+                (0x23, 0), -- HMM1
+                (0x24, 0), -- HMBL
+                (0x25, 0), -- VDELP0
+                (0x26, 0), -- VDELP1
+                (0x27, 0), -- VDELBL
+                (0x28, 0), -- RESMP0
+                (0x29, 0), -- RESMP1
+                (0x2a, 0), -- HMOVE
+                (0x2b, 0), -- HMCLR
+                (0x2c, 0)  -- CXCLR
+            ]
+
 main :: IO ()
 main = do
     args' <- cmdArgs clargs
@@ -79,7 +121,7 @@ main = do
                        recordArray
 #endif
                        initBankState romArray
-                       0x0000 window prog attrib tex' textureData'
+                       0x0000 window prog attrib tex' textureData' delayList
 
     let loop = do
             events <- liftIO $ SDL.pollEvents
--- src/Emulation.hs
+++ src/Emulation.hs
@@ -78,12 +78,13 @@ initState :: Int -> Int -> Int -> Int ->
              GL.AttribLocation ->
              GL.TextureObject ->
              Ptr Word8 ->
+             [(Word16, Int)] ->
              IO Atari2600
 initState xscale' yscale' width height ram'
 #if TRACE
             record'
 #endif
-            initBankState rom' initialPC window prog attrib initTex initTextureData = do
+            initBankState rom' initialPC window prog attrib initTex initTextureData delayList = do
           stellaDebug' <- newIORef DebugState.start
           bankState' <- newIORef initBankState
           clock' <- newIORef 0
@@ -123,46 +124,7 @@ initState xscale' yscale' width height ram'
               _tex = initTex,
               _glProg = prog,
               _glAttrib = attrib,
-              _delays = U.listArray (0, 0x2c) (replicate (0x2c+1) 0) U.// [
-                (0x00, 0), -- VSYNC
-                (0x01, 0), -- VBLANK
-                (0x02, 0), -- WSYNC
-                (0x03, 0), -- NUSIZ0
-                (0x05, 4), -- NUSIZ1
-                (0x06, 0), -- COLUP0
-                (0x07, 0), -- COLUP0
-                (0x08, 0), -- COLUPF
-                (0x09, 0), -- COLUBK
-                (0x0a, 0), -- CTRLPF
-                (0x0b, 0), -- REFP0
-                (0x0c, 0), -- REFP1
-                (0x0d, 3), -- PF0
-                (0x0e, 3), -- PF1
-                (0x0f, 3), -- PF2
-                (0x10, 5), -- RESP0
-                (0x11, 5), -- RESP1
-                (0x12, 4), -- RESM0
-                (0x13, 4), -- RESM1
-                (0x14, 4), -- RESBL
-                (0x1b, 1), -- GRP0
-                (0x1c, 1), -- GRP1
-                (0x1d, 0), -- ENAM0
-                (0x1e, 0), -- ENAM1
-                (0x1f, 0), -- ENABL
-                (0x20, 0), -- HMP0
-                (0x21, 0), -- HMP1
-                (0x22, 0), -- HMM0
-                (0x23, 0), -- HMM1
-                (0x24, 0), -- HMBL
-                (0x25, 0), -- VDELP0
-                (0x26, 0), -- VDELP1
-                (0x27, 0), -- VDELBL
-                (0x28, 0), -- RESMP0
-                (0x29, 0), -- RESMP1
-                (0x2a, 0), -- HMOVE
-                (0x2b, 0), -- HMCLR
-                (0x2c, 0)  -- CXCLR
-                ]
+              _delays = U.listArray (0, 0x2c) (replicate (0x2c+1) 0) U.// delayList
           }
 
 {- INLINE stellaHmclr -}
