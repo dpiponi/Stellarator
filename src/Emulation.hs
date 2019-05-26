f40f211987ecef243463f8222273ddcef55e68d7
Dan Piponi <dpiponi@gmail.com>
2019-05-25 17:53:50 -0700

    Removed some commented out code

--- src/Emulation.hs
+++ src/Emulation.hs
@@ -5,29 +5,14 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE ApplicativeDo #-}
 
--- module Emulation(stellaDebug,
---                  dumpRegisters,
---                  dumpState,
---                  clock,
---                  stellaClock,
---                  initState,
---                  trigger1,
---                  trigger2,
---                  loopUntil,
---                  initHardware,
---                  load,
---                  getA) where
-
 module Emulation where
 
 import Asm hiding (a, s)
 import Atari2600
--- import BitManips
 import Control.Lens hiding (set, op, index)
 import Control.Monad.Reader
 import Data.Array.IO hiding (index)
 import Data.Bits hiding (bit)
--- import Data.Bits.Lens
 import Data.IORef
 import Data.Int
 import CPU
@@ -527,24 +512,6 @@ withAbsX op = do
     dst <- op src
     writeMemory addrX dst
 
--- -- {-# INLINABLE setN #-}
--- setN :: Word8 -> MonadAtari ()
--- setN r = putN $ r >= 0x80
--- 
--- {-# INLINABLE setZ #-}
--- setZ :: Word8 -> MonadAtari ()
--- setZ r = putZ $ r == 0
--- 
--- -- {-# INLINABLE setNZ #-}
--- setNZ :: Word8 -> MonadAtari Word8
--- setNZ r = setN r >> setZ r >> return r
--- 
--- -- {-# INLINABLE setNZ_ #-}
--- setNZ_ :: Word8 -> MonadAtari ()
--- setNZ_ r = setN r >> setZ r
--- discard :: MonadAtari Word8 -> MonadAtari ()
--- discard = void
-
 -- 7 clock cycles
 -- {-# INLINABLE brk #-}
 brk :: MonadAtari ()
@@ -717,26 +684,6 @@ rts = do
     tick 1
     discard $ readMemory p0
     putPC (p0+1)
--- 
--- timerTick' :: Word8 -> Int -> Int -> Word8 -> (Word8, Int, Int, Word8)
--- timerTick' 0      0         _         _       = (-1,       3*1-1,         1,         0x80)
--- timerTick' intim' 0         interval' timint' = (intim'-1, 3*interval'-1, interval', timint')
--- timerTick' intim' subtimer' interval' timint' = (intim',   subtimer'-1,   interval', timint')
--- 
--- timerTick :: MonadAtari ()
--- timerTick = do
---     (intim'', subtimer'', interval'', timint'') <- timerTick' <$> load intim <*> load subtimer <*> load interval <*> load timint
---     intim @= intim''
---     subtimer @= subtimer''
---     interval @= interval''
---     timint @= timint''
-
--- startIntervalTimerN :: Int -> Word8 -> MonadAtari ()
--- startIntervalTimerN n v = do
---     interval @= n
---     subtimer @= 0 -- Was 3*n-1
---     intim @= v
---     timint @= 0
 
 makeDelayArray:: [(Word16, Int)] -> IO (IOUArray Word16 Int)
 makeDelayArray delayList = do
@@ -818,46 +765,6 @@ initState xscale' yscale' width height ram'
               _glAttrib = attrib,
               _delays = delayArray
           }
--- 
--- -- {- INLINE stellaHmclr -}
--- stellaHmclr :: MonadAtari ()
--- stellaHmclr =
---     mapM_ (@= 0) [hmp0, hmp1, hmm0, hmm1, hmbl]
-
--- {- INLINE stellaCxclr -}
--- stellaCxclr :: MonadAtari ()
--- stellaCxclr =
---     mapM_ (@= 0) [cxm0p, cxm1p, cxm0fb, cxm1fb, cxp0fb, cxp1fb, cxblpf, cxppmm]
--- 
--- -- {- INLINE stellaHmove -}
--- stellaHmove :: MonadAtari ()
--- stellaHmove = do
---     pendingHmove @= True
--- 
---     poffset0 <- load hmp0
---     modify ppos0 $ \ppos0' ->  wrap160 (ppos0'-clockMove poffset0)
--- 
---     poffset1 <- load hmp1
---     modify ppos1 $ \ppos1' ->  wrap160 (ppos1'-clockMove poffset1)
--- 
---     moffset0 <- load hmm0
---     modify mpos0 $ \mpos0' ->  wrap160 (mpos0'-clockMove moffset0)
--- 
---     moffset1 <- load hmm1
---     modify mpos1 $ \mpos1' ->  wrap160 (mpos1'-clockMove moffset1)
--- 
---     boffset <- load hmbl
---     modify bpos $ \bpos' -> wrap160 (bpos'-clockMove boffset)
--- 
--- -- {-# INLINE wrap160 #-}
--- wrap160 :: Int -> Int
--- wrap160 i | i < picx = wrap160 (i+160)
---           | i >= picx+160 = wrap160 (i-160)
--- wrap160 i = i
-
--- -- {-# INLINE iz #-}
--- iz :: Word16 -> Int -- or NUM
--- iz = fromIntegral
 
 {-
 Here's a standard kernel:
